e1458a3ec8b4aad24142e8a1be480a2d1eaf786d
Zainulabiddin1 <zainulabiddingujjar321@gmail.com>
2023-07-15 17:49:38 +0500

    Middleware_examples

--- MyBookStoreApplication/MyBookStoreApplication/Program.cs
+++ MyBookStoreApplication/MyBookStoreApplication/Program.cs
@@ -1,7 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
-
 app.Run();
- 
+
+//app.UseRouting();
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapGet("/", async context =>
+//    await context.Response.WriteAsync("Hello World!")
+//    );
+//});
+//app.Run();
